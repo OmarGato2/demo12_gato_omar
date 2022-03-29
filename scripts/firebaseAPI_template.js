2204d3c1ac0a5f1835218ae71d0d052eca6e688a
Omar Gato <ogato@my.bcit.com>
2022-03-29 10:35:08 -0700

    demo 12 complete

--- scripts/firebaseAPI_template.js
+++ scripts/firebaseAPI_template.js
@@ -1,13 +1,14 @@
 //----------------------------------------
 //  Your web app's Firebase configuration
 //----------------------------------------
-var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+const firebaseConfig = {
+    apiKey: "AIzaSyCOZe_Ng2zSVVuN_W_HO45GB_l_KOs43-g",
+    authDomain: "fir-comp-1800-8feea.firebaseapp.com",
+    projectId: "fir-comp-1800-8feea",
+    storageBucket: "fir-comp-1800-8feea.appspot.com",
+    messagingSenderId: "215046265331",
+    appId: "1:215046265331:web:8ed9a8c06c8449c9501a86",
+    measurementId: "G-L5303GWLHK"
 };
 
 //--------------------------------------------
